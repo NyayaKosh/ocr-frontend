b5414a2d3ec827b62b74a1a98a6675f92c0d1287
vikas mandal <vikas.mandal@coreops.ai>
2025-11-17 07:02:55 +0530

    updated in auth callback

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,18 +1,27 @@
 import { createClient } from "@/lib/supabase/server";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
-    const url = new URL(request.url);
-    const code = url.searchParams.get("code");
-
+export async function GET(request: Request) {
+    const { searchParams, origin } = new URL(request.url)
+    const code = searchParams.get('code')
+    let next = searchParams.get('next') ?? '/'
+    if (!next.startsWith('/')) {
+        next = '/'
+    }
     if (code) {
-        const supabase = createClient();
-        const { error } = await supabase.auth.exchangeCodeForSession(code);
-
+        const supabase = createClient()
+        const { error } = await supabase.auth.exchangeCodeForSession(code)
         if (!error) {
-            const response = NextResponse.redirect(request.nextUrl.origin);
-            return response
+            const forwardedHost = request.headers.get('x-forwarded-host')
+            const isLocalEnv = process.env.NODE_ENV === 'development'
+            if (isLocalEnv) {
+                return NextResponse.redirect(`${origin}${next}`)
+            } else if (forwardedHost) {
+                return NextResponse.redirect(`https://${forwardedHost}${next}`)
+            } else {
+                return NextResponse.redirect(`${origin}${next}`)
+        }
         }
     }
-    return NextResponse.redirect(new URL(`${request.nextUrl.origin}/login?error=callback_error`, request.url));
+    return NextResponse.redirect(`${origin}/auth/auth-code-error`)
 }
